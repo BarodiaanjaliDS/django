65a137605efcebdd2c2f3eff810a9bee9bcee305
Anjali <barodiaanjali@gmail.com>
2023-04-24 01:27:10 +0530

    correct Allowed host to '*' for prod env

--- office_emp_project/settings.py
+++ office_emp_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6gl-@$zic*0_dl=o7$wvt2z%(gk7gd=xnqnk^fo9e!jo7nn#ir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*x`']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
